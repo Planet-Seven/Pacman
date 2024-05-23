a24fe0655e85de75da3f1db7c6c4426d7d11649e
Vojtěch Svoboda <svobov25@fit.cvut.cz>
2024-05-23 12:41:37 +0200

    seme: commit of config.h

---
+++ progtest/seme/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+constexpr int PLAYER_SPEED = 4;
+
+constexpr int INITIAL_POWERUP_TIME = 5;
+constexpr int POWER_UP_TIME_DECREMENT = 1;
+
+constexpr int TIME_BETWEEN_GUARD_MODE = 30;
+constexpr int INITIAL_GUARD_TIME = 10;
+constexpr int GUARD_TIME_DECREMENT = 1;
+
+constexpr int WINDOW_SCALE = 30;
+constexpr int BOTTOM_PADDING = 100;
+constexpr int FONT_SIZE = 20;
